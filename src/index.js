9ef6824dac2e18ee48bd8f7492d998d27e0a358d
richardg91_bien <richardg91@gmail.com>
2020-06-18 02:59:40 -0300

    version 1

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRoute = require('./routes/index');
+
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+
+
+
+
+//middlewares
+
+
+// routes
+app.use(require('./routes/index'));
+
+// static files
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get ('port'));
+});
